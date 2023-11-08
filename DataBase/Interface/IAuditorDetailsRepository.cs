fcaf7f08c6f0cc6151351d4bfa2482090a74d031
amarkantsem <amarkant.semwal@newvision-software.com>
2023-11-08 11:43:10 +0530

    comit all

---
+++ DataBase/Interface/IAuditorDetailsRepository.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Interface
+{
+    public interface IAuditorDetailsRepository: IGenericRepository<AuditorsDetails>
+    {
+
+        public Task<AuditorsDetails> AddAuditorsDetails(AuditorsDetails  auditorsDetails);
+    
+    }
+}
