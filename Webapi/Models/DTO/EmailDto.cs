7b0194f1cfe032048ca297578036d4538bf99a00
amarkantsem <amarkant.semwal@newvision-software.com>
2023-11-08 14:17:27 +0530

    remove files

--- Webapi/Models/DTO/EmailDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CodePulse.API.Models.DTO
-{
-    public class EmailDto
-    {
-        public string To { get; set; } = string.Empty;
-        public string Subject { get; set; } = string.Empty;
-        public string Body { get; set; } = string.Empty;
-    }
-}
--- Webapi/Services/EmailService/IEmailService.cs
+++
@@ -1,9 +0,0 @@
-﻿using CodePulse.API.Models.DTO;
-
-namespace CodePulse.API.Services.EmailService
-{
-    public interface IEmailService
-    {
-        Task SendEmail(EmailDto emailRequest);
-    }
-}
