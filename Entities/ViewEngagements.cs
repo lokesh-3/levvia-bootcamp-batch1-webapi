f7ecdd0e7720451bee099c0640ba3bf14cce74a9
PriteshTapkir <pritesh.tapkir@newvision-software.com>
2023-11-22 21:39:26 +0530

    ViewEngagements API

---
+++ Entities/ViewEngagements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class ViewEngagements
+    {
+        public string ClientName { get; set; }
+        public int ClientId { get; set; }
+        public string EngagementStartDate { get; set; }
+        public string EngagementEndDate { get; set; }
+        public String AuditName { get; set; }
+        public String AuditStatus { get; set; }
+    }
+}
